2ff7bdd15bf1e16c61b287620ffa531fe98ad30f
Simon McCabe <5106638+7s26simon@users.noreply.github.com>
2025-12-29 12:51:17 +0000

    Update main.js
    Maintain persistence of hidden fields. Off/On functionality.

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-// No tabs or host permissions needed!
-
+function applyHiddenFields(enabled) {
     chrome.tabs.executeScript({
         code: `
             function hasClass(a,b){
                 return(" "+a.className+" ").indexOf(" "+b+" ")>-1
             }
-
-            for(var arr=document.getElementsByTagName("input"),form_hacked="form_hacked",hidden_inputs=!0,i=0;i<arr.length;i++)
-            
-                hasClass(arr[i],form_hacked)?
-                    (arr[i].setAttribute("type","hidden"),
-                            elem = document.getElementById("explainText"),
-                            arr[i].parentNode.removeChild(elem),
-                            arr[i].classList.remove("form_hacked"),
-                            hidden_inputs=!0
-                    ):
-                "hidden"==arr[i].type&&(
-                    arr[i].setAttribute("type","text"),
-                        arr[i].classList.add("form_hacked"),            
-                        explainText = arr[i].getAttribute("name") ? arr[i].getAttribute("name") : (arr[i].id ? arr[i].id : "(not-set)"),
-                        arr[i].insertAdjacentHTML("beforebegin", "<div id='explainText' style='color: gray;display: block;z-index: 999; '>" + explainText + ":</div>"),
-                        hidden_inputs=!1
-                );
+            for(var arr=document.getElementsByTagName("input"),form_hacked="form_hacked",i=0;i<arr.length;i++){
+                if(hasClass(arr[i],form_hacked)){
+                    ${enabled ? '' : `
+                        arr[i].setAttribute("type","hidden");
+                        let elem = document.getElementById("explainText");
+                        if(elem) elem.remove();
+                        arr[i].classList.remove("form_hacked");
+                    `}
+                } else if(arr[i].type==="hidden"){
+                    ${enabled ? `
+                        arr[i].setAttribute("type","text");
+                        arr[i].classList.add("form_hacked");            
+                        let explainText = arr[i].getAttribute("name") || arr[i].id || "(not-set)";
+                        arr[i].insertAdjacentHTML("beforebegin", "<div id='explainText' style='color: gray;display: block;z-index: 999; '>" + explainText + ":</div>");
+                    ` : ''}
+                }
+            }
         `
+    });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.storage.local.get(['enabled'], function(result) {
+        let enabled = !result.enabled;
+        chrome.storage.local.set({enabled: enabled}, function() {
+            applyHiddenFields(enabled);
+        });
     });
 });
